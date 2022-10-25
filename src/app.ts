44a43e0f2f002355ed781844b26dba18f35b71ed
Claudio <szclsb@gmail.com>
2022-10-25 19:17:19 +0200

    integrated pug

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,17 @@
 import express from "express";
+import pug from "pug";
+import * as path from "path";
 
 const app = express();
 
+app.set('views', path.join(__dirname, "assets", "views"))
+app.set('view engine', 'pug')
 app.listen(8080, () => {
     app.get("/", (req, res) => {
-        res.send("hello world");
+        res.render('index', {
+            title: 'Hey',
+            message: 'Hello there!'
+        });
     })
     console.log( `server started at http://localhost:8080`);
 })
