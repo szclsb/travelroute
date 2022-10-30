9fa26808c5c72c308f6a2fd403e3ca7be984ca15
Claudio <szclsb@gmail.com>
2022-10-30 11:34:01 +0100

    integrated openstreetmap

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,26 @@ import * as path from "path";
 
 const app = express();
 
+const openstreetmap = 'https://www.openstreetmap.org';
+const contentPolicy = {
+    'default-src': `'self'`,
+    'font-src': `'self'`,
+    'img-src': `'self'`,
+    'script-src': `'self'`,
+    'style-src': `'self' ${openstreetmap}`,
+    'frame-src': `'self' ${openstreetmap}`,
+}
 app.use('/static', express.static(__dirname + '/public'));
+app.use('/favicon.ico', express.static(__dirname + '/public/favicon.ico'));
+app.use((req, res, next) => {
+    res.setHeader(
+        'Content-Security-Policy-Report-Only',
+        Object.entries(contentPolicy)
+            .map(([key, value]) => `${key} ${value}`)
+            .reduce((left, right) => `${left};${right}`)
+    );
+    next();
+});
 app.set('views', path.join(__dirname, "views"))
 app.set('view engine', 'pug')
 app.listen(8080, () => {
